c8b36b3dc49a57454a94f1585b7dbca69566910a
Claus W <138636927+Imaginectop@users.noreply.github.com>
2024-03-21 18:52:15 +0300

    Update image_processing_service.py

--- image_processing_service.py
+++ image_processing_service.py
@@ -2,41 +2,61 @@ import cv2
 import os
 import numpy as np
 from face_detection_service import detect_faces
+import logging
 
 TEMP_FILES_DIR = "temp_files"
 
+# Настройка логгирования
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 def apply_mask(image_path: str, mask_path: str) -> (str, bool):
-    image = cv2.imread(image_path)
-    mask = cv2.imread(mask_path, cv2.IMREAD_UNCHANGED)
+    if not os.path.exists(image_path):
+        logger.error(f"Файл изображения {image_path} не найден.")
+        return None, False
+    
+    if not os.path.exists(mask_path):
+        logger.error(f"Файл маски {mask_path} не найден.")
+        return None, False
+
+    try:
+        image = cv2.imread(image_path)
+        if image is None:
+            raise FileNotFoundError(f"Файл изображения {image_path} не может быть прочитан.")
+        mask = cv2.imread(mask_path, cv2.IMREAD_UNCHANGED)
+        if mask is None:
+            raise FileNotFoundError(f"Файл маски {mask_path} не может быть прочитан.")
+    except FileNotFoundError as e:
+        logger.error(f"Ошибка при загрузке файла: {e}")
+        return None, False
+    except Exception as e:
+        logger.error(f"Неизвестная ошибка при чтении файла: {e}")
+        return None, False
 
     faces = detect_faces(image_path)
 
-    # Если лица не обнаружены, возвращаем путь к исходному изображению и False
     if not faces:
         return image_path, False
 
-    # В данной реализации мы применяем маску только к первому обнаруженному лицу
     (startX, startY, endX, endY) = faces[0]
     face_width = endX - startX
     face_height = endY - startY
 
-    # Увеличиваем размер маски на 10%
     new_width = int(face_width * 1.1)
     new_height = int(face_height * 1.1)
 
-    # Масштабируем маску
-    mask_resized = cv2.resize(mask, (new_width, new_height))
+    try:
+        mask_resized = cv2.resize(mask, (new_width, new_height))
+    except Exception as e:
+        logger.error(f"Ошибка при изменении размера маски: {e}")
+        return None, False
 
-    # Центрирование маски относительно лица
     dx = (new_width - face_width) // 2
     dy = (new_height - face_height) // 2
 
-    # Накладываем маску на изображение
     for i in range(mask_resized.shape[0]):
         for j in range(mask_resized.shape[1]):
-            if mask_resized[i, j, 3] > 0:  # Если маска не прозрачна
-                # Проверка границ перед присваиванием
+            if mask_resized[i, j, 3] > 0:
                 if 0 <= startY + i - dy < image.shape[0] and 0 <= startX + j - dx < image.shape[1]:
                     image[startY + i - dy, startX + j - dx, :] = mask_resized[i, j, :-1]
 
